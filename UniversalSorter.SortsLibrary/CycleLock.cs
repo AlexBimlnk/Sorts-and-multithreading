914acaab81f159b6a8ed5bbdc411d6ddb5453ff8
Aleksandr Illarionov <aleksandr.illarionov@altenar.com>
2022-08-06 16:31:26 +0300

    Merge refactor

---
+++ UniversalSorter.SortsLibrary/CycleLock.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+
+namespace UniversalSorter.SortsLibrary;
+
+internal struct CycleLock
+{
+    private int _resourseInUse;
+
+    public void Enter()
+    {
+        while (true)
+        {
+            if (Interlocked.Exchange(ref _resourseInUse, 1) == 0)
+                return;
+        }
+    }
+
+    public void Leave()
+    {
+        Volatile.Write(ref _resourseInUse, 0);
+    }
+}
--- UniversalSorter.SortsLibrary/SortAlgorithms/MergeSort.cs
+++ UniversalSorter.SortsLibrary/SortAlgorithms/MergeSort.cs
@@ -1,118 +1,113 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
-namespace UniversalSorter.SortsLibrary.SortAlgorithms
+namespace UniversalSorter.SortsLibrary.SortAlgorithms;
+
+/// <summary>
+/// Сортировка слиянием.
+/// </summary>
+public class MergeSort<T> : AlgorithmBase<T> where T : IComparable
 {
-    /// <summary>
-    /// Сортировка слиянием.
-    /// </summary>
-    public class MergeSort<T> : AlgorithmBase<T> where T : IComparable
+    private int _currentThreads = 1;
+    private readonly CycleLock _cycleLock = new();
+
+    public MergeSort(IEnumerable<T> items) : base(items) { }
+    public MergeSort(IEnumerable<T> items, int countThreads) : base(items, countThreads) { }
+
+    public override ThreadSupport ThreadSupport => ThreadSupport.Infinity;
+
+    private void Merge(List<T> left, List<T> right, List<T> outputItems)
     {
-        private int _currentThreads = 1;
-        public override ThreadSupport ThreadSupport => ThreadSupport.Infinity;
+        int leftCounter = 0; int rightCounter = 0; int outputCounter = 0;
 
-
-        public MergeSort(IEnumerable<T> items) : base(items) { }
-        public MergeSort(IEnumerable<T> items, int countThreads) : base(items, countThreads) { }
-
-
-        public override void StartSort()
+        while (leftCounter < left.Count && rightCounter < right.Count)
         {
-            Sort(collection);
-        }
-        public override Task StartMultiThreadingSort()
-        {
-            SortWithThreads(collection);
-            _currentThreads = 1;
-            return Task.CompletedTask;
-        }
-
-
-        private void SortWithThreads(List<T> items)
-        {
-            if (items.Count() == 1)
-                return;
-
-            int mid = items.Count() / 2;
-
-            var L = items.Take(mid).ToList();
-            var R = items.Skip(mid).ToList();
-
-            Task leftSortThread = null;
-            Task rightSortThread = null;
-
-            if (_currentThreads < Threads)
+            if (Compare(left[leftCounter], right[rightCounter]) == -1)
             {
-                leftSortThread = new Task(()=>SortWithThreads(L));
-                rightSortThread = new Task(() => SortWithThreads(R));
-                _currentThreads++;
-            }
-
-            leftSortThread?.Start();
-            rightSortThread?.Start();
-
-            if (leftSortThread == null)
-            {
-                SortWithThreads(L);
-                SortWithThreads(R);
+                Set(left[leftCounter], outputCounter, outputItems);
+                leftCounter++;
             }
             else
             {
-                Task.WaitAll(leftSortThread, rightSortThread);
+                Set(right[rightCounter], outputCounter, outputItems);
+                rightCounter++;
             }
-
-            Merge(L, R, items);
+            outputCounter++;
         }
-        private void Sort(List<T> items)
+
+        while (leftCounter < left.Count)
         {
-            if (items.Count() == 1)
-                return;
+            Set(left[leftCounter], outputCounter, outputItems);
+            leftCounter++; outputCounter++;
+        }
+        while (rightCounter < right.Count)
+        {
+            Set(right[rightCounter], outputCounter, outputItems);
+            rightCounter++; outputCounter++;
+        }
+    }
+    private void Sort(List<T> items)
+    {
+        if (items.Count == 1)
+            return;
 
-            int mid = items.Count() / 2;
+        int mid = items.Count / 2;
 
-            var L = items.Take(mid).ToList();
-            var R = items.Skip(mid).ToList();
+        var L = items.Take(mid).ToList();
+        var R = items.Skip(mid).ToList();
 
+        Sort(L);
+        Sort(R);
+
+        Merge(L, R, items);
+    }
+
+    private async Task SortWithThreads(List<T> items)
+    {
+        if (items.Count == 1)
+            return;
+
+        int mid = items.Count / 2;
+
+        var L = items.Take(mid).ToList();
+        var R = items.Skip(mid).ToList();
+
+        bool continueInSingleThread = true;
+
+        _cycleLock.Enter();
+
+        if (_currentThreads < Threads)
+        {
+            _currentThreads++;
+            continueInSingleThread = false;
+        }
+
+        _cycleLock.Leave();
+
+        if (continueInSingleThread)
+        {
             Sort(L);
             Sort(R);
-
-            Merge(L, R, items);
         }
-
-
-        private void Merge(List<T> left, List<T> right, List<T> outputItems)
+        else
         {
-            int leftCounter = 0; int rightCounter = 0; int outputCounter = 0;
-
-            while (leftCounter < left.Count && rightCounter < right.Count)
-            {
-                if (Compare(left[leftCounter], right[rightCounter]) == -1)
-                {
-                    Set(left[leftCounter], outputCounter, outputItems);
-                    leftCounter++;
-                }
-                else
-                {
-                    Set(right[rightCounter], outputCounter, outputItems);
-                    rightCounter++;
-                }
-                outputCounter++;
-            }
-
-            while (leftCounter < left.Count)
-            {
-                Set(left[leftCounter], outputCounter, outputItems);
-                leftCounter++; outputCounter++;
-            }
-            while (rightCounter < right.Count)
-            {
-                Set(right[rightCounter], outputCounter, outputItems);
-                rightCounter++; outputCounter++;
-            }
+            await Task.WhenAll(
+                    Task.Run(() => SortWithThreads(L)),
+                    Task.Run(() => SortWithThreads(R)));
         }
+
+        Merge(L, R, items);
+    }
+
+    public override void StartSort()
+    {
+        Sort(collection);
+    }
+    public override Task StartMultiThreadingSort()
+    {
+        _currentThreads = 1;
+        return SortWithThreads(collection);
     }
 }
