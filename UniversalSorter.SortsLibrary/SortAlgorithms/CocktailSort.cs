bacdac291379f6c1f24b0e60e8df80271dafd3a3
Aleksandr Illarionov <aleksandr.illarionov@altenar.com>
2022-08-06 15:28:23 +0300

    Cocktail refactor

--- UniversalSorter.SortsLibrary/SortAlgorithms/CocktailSort.cs
+++ UniversalSorter.SortsLibrary/SortAlgorithms/CocktailSort.cs
@@ -1,86 +1,80 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
-namespace UniversalSorter.SortsLibrary.SortAlgorithms
+namespace UniversalSorter.SortsLibrary.SortAlgorithms;
+
+public class CocktailSort<T> : AlgorithmBase<T> where T : IComparable
 {
-    public class CocktailSort<T> : AlgorithmBase<T> where T : IComparable
+    public CocktailSort(IEnumerable<T> items) : base(items) { }
+    public CocktailSort(IEnumerable<T> items, int countThreads) : base(items, countThreads) { }
+
+    public override ThreadSupport ThreadSupport => ThreadSupport.Infinity;
+
+    private void Sort(int start, int end)
     {
-        public override ThreadSupport ThreadSupport => ThreadSupport.Infinity;
+        int leftPointer = start;
+        int rightPointer = end - 1;
 
-        public CocktailSort(IEnumerable<T> items) : base(items) { }
-        public CocktailSort(IEnumerable<T> items, int countThreads) : base(items, countThreads) { }
-
-
-        public override void StartSort()
+        while (leftPointer < rightPointer)
         {
-            Sort(0, collection.Count);
-        }
-        public override Task StartMultiThreadingSort()
-        {
-            int chunk = collection.Count / Threads;
-            List<Task> tasks = new List<Task>();
-            int i = 0;
-            for (; i < Threads; i++)
+            var swapCount = 0;
+
+            for (int i = leftPointer; i < rightPointer; i++)
             {
-                int start = i * chunk;
-                int end = start + chunk;
-                tasks.Add(new Task(() => Sort(start, end)));
-                tasks[i].Start();
-            }
-            tasks.Add(new Task(() => Sort(i * chunk, collection.Count)));
-            tasks.Last().Start();
-
-            Task.WaitAll(tasks.ToArray());
-
-
-            MergeChunks(chunk);
-
-            return Task.CompletedTask;
-        }
-
-        
-        private void Sort(int start, int end)
-        {
-            int leftPointer = start;
-            int rightPointer = end - 1;
-
-            while (leftPointer < rightPointer)
-            {
-                var swapCount = 0;
-
-                for (int i = leftPointer; i < rightPointer; i++)
+                if (Compare(collection[i], collection[i + 1]) == 1)
                 {
-                    if (Compare(collection[i], collection[i + 1]) == 1)
-                    {
-                        Swap(i, i + 1);
-                        swapCount++;
-                    }
-                }
-                rightPointer--;
-
-                if (swapCount == 0)
-                {
-                    break;
-                }
-
-                for (int i = rightPointer; i > leftPointer; i--)
-                {
-                    if (Compare(collection[i], collection[i - 1]) == -1)
-                    {
-                        Swap(i, i - 1);
-                        swapCount++;
-                    }
-                }
-                leftPointer++;
-
-                if (swapCount == 0)
-                {
-                    break;
+                    Swap(i, i + 1);
+                    swapCount++;
                 }
             }
+            rightPointer--;
+
+            if (swapCount == 0)
+            {
+                break;
+            }
+
+            for (int i = rightPointer; i > leftPointer; i--)
+            {
+                if (Compare(collection[i], collection[i - 1]) == -1)
+                {
+                    Swap(i, i - 1);
+                    swapCount++;
+                }
+            }
+            leftPointer++;
+
+            if (swapCount == 0)
+            {
+                break;
+            }
         }
+    }
+
+    public override void StartSort()
+    {
+        Sort(0, collection.Count);
+    }
+
+    public override async Task StartMultiThreadingSort()
+    {
+        int chunk = collection.Count / Threads;
+
+        await Task.WhenAll(
+                Enumerable.Range(0, Threads)
+                    .Select(x =>
+                    {
+                        var start = x * chunk;
+                        var end = start + chunk;
+
+                        return Task.Run(() => Sort(start, end));
+                    }));
+
+        await Task.Run(() => Sort(Threads * chunk, collection.Count));
+
+
+        MergeChunks(chunk);
     }
 }
