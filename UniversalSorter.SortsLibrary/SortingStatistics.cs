d95025318ae44a965c7f387288fa32a746c11e3f
Aleksandr Illarionov <aleksandr.illarionov@altenar.com>
2022-08-06 17:26:27 +0300

    stat refactor

--- UniversalSorter.SortsLibrary/SortingStatistics.cs
+++ UniversalSorter.SortsLibrary/SortingStatistics.cs
@@ -3,48 +3,46 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
-using System.Threading;
 
 namespace UniversalSorter.SortsLibrary
 {
     public class SortingStatistics<T> where T : IComparable
     {
-        private const int _minThreadsCount = 2;
-
-        private Stopwatch _stopwatch = new Stopwatch();
-        private AlgorithmBase<T> _algorithm;
-
-        private List<T> _items = new List<T>();
-        private TimeSpan _timeWork;
-        private double _memoryUsed = 0;
-        private int _maxThreadsCount = 2;
         private int _stepIterationThreads = 1;
-        private Dictionary<int, Tuple<TimeSpan, double>> _threadsTimeMemory = new Dictionary<int, Tuple<TimeSpan, double>>();
+        private TimeSpan _timeWork;
 
-        private StringBuilder _information = new StringBuilder();
+        private readonly List<T> _items = new();
 
+        private readonly AlgorithmBase<T> _algorithm;
 
-        /// <summary>
-        /// Возвращает время потраченное на сортировку коллекции.
-        /// </summary>
+        private readonly Stopwatch _stopwatch = new();
+        private readonly Dictionary<int, TimeSpan> _threadsTimeMemory = new();
 
-        public TimeSpan TimeWork => _timeWork;
-        public double TimeWorkInMs => _timeWork.Milliseconds;
-        public double MemoryUsed => _memoryUsed;
-        public Dictionary<int, Tuple<TimeSpan, double>> GetTreadsTimeMemory => _threadsTimeMemory;
-        public int ItemsCount { get; private set; }
-        public int MinThreadsCount => _minThreadsCount;
-        public int MaxThreadsCount
+        private readonly StringBuilder _information = new();
+
+        public SortingStatistics(
+            AlgorithmBase<T> algorithm,
+            int maxThreadsCount = 2,
+            int stepIterationThreads = 1)
         {
-            get { return _maxThreadsCount; }
-            set
-            {
-                if (2 <= MaxThreadsCount)
-                {
-                    _maxThreadsCount = value;
-                }
-            }
+            _algorithm = algorithm ?? throw new ArgumentNullException(nameof(algorithm));
+            _items.AddRange(algorithm.Items);
+            MaxThreadsCount = maxThreadsCount;
+            StepIterationThreads = stepIterationThreads;
+
+            _information.AppendLine($"General settings:\n" +
+                                    $"\tSort type: {_algorithm.GetType().Name}\n" +
+                                    $"\tSort support thread: {_algorithm.ThreadSupport}\n" +
+                                    $"\tItems count: {ItemsCount}\n" +
+                                    $"\tMaximum threads: {MaxThreadsCount}\n" +
+                                    $"\tStep iteration threads: {_stepIterationThreads}");
         }
+
+        public IReadOnlyDictionary<int, TimeSpan> GetTreadsTimeMemory => _threadsTimeMemory;
+        public int ItemsCount => _algorithm.Items.Count;
+        public int MinThreadsCount => 1;
+        public int MaxThreadsCount { get; set; }
+
         /// <summary>
         /// Шаг итерационного начисления потоков для сбора статистики
         /// влияния кол-ва потоков на сортировку.
@@ -54,107 +52,47 @@ namespace UniversalSorter.SortsLibrary
             get { return _stepIterationThreads; }
             set
             {
-                if (0 < value && value < _maxThreadsCount)
+                if (0 < value && value < MaxThreadsCount)
                 {
                     _stepIterationThreads = value;
                 }
             }
         }
 
-        public static StringBuilder GetStatistics(SortingStatistics<T> statistics)
-        {
-            return statistics.GetStatistics();
-        }
-
-        public SortingStatistics(AlgorithmBase<T> algorithm, int maxThreadsCount = 2,
-                                 int stepIterationThreads = 1)
-        {
-            _algorithm = algorithm;
-            _items.AddRange(algorithm.Items);
-            ItemsCount = algorithm.Items.Count;
-            MaxThreadsCount = maxThreadsCount;
-            StepIterationThreads = stepIterationThreads;
-
-            _information.AppendLine($"General settings:\n" +
-                                    $"\tSort type: {_algorithm.GetType().Name}\n" +
-                                    $"\tSort support thread: {_algorithm.ThreadSupport}\n" +
-                                    $"\tItems count: {ItemsCount}\n" +
-                                    $"\tMaximum threads: {_maxThreadsCount}\n" +
-                                    $"\tStep iteration threads: {_stepIterationThreads}");
-        }
-
-
-        public void StartCollectingStatistics()
-        {
-            StatisticsFromMethod(() => _algorithm.StartSort());
-
-            _information.AppendLine($"Threads: 1\n" +
-                                    $"\tTime work: {_timeWork.Milliseconds}ms\n" +
-                                    $"\tMemory used: {_memoryUsed}MByte");
-
-            ResetItems();
-
-            Thread.Sleep(500);
-
-            for (int i = _minThreadsCount; i <= MaxThreadsCount; i += _stepIterationThreads)
-            {
-                _algorithm.Threads = i;
-                StatisticsFromMethod(() => _algorithm.StartMultiThreadingSort());
-
-                _threadsTimeMemory.Add(i, new Tuple<TimeSpan, double>(_timeWork, _memoryUsed));
-
-                _information.AppendLine($"Threads: {i}\n" +
-                                        $"\tTime work: {_timeWork.Milliseconds}ms\n" +
-                                        $"\tMemory used: {_memoryUsed}MByte");
-
-                ResetItems();
-
-                Thread.Sleep(500);
-            }
-
-        }
-
-        public StringBuilder GetStatistics()
-        {
-            return _information;
-        }
-
-        public int BetterThreadsCountOfTime()
-        {
-            TimeSpan minTime = _threadsTimeMemory.Values.ToList()[0].Item1;
-            int betterThreadsCount = _threadsTimeMemory.Keys.ToList()[0];
-
-            foreach (var i in _threadsTimeMemory.Keys)
-            {
-                if (_threadsTimeMemory[i].Item1 < minTime)
-                {
-                    minTime = _threadsTimeMemory[i].Item1;
-                    betterThreadsCount = i;
-                }
-            }
-
-            return betterThreadsCount;
-        }
-
         private void StatisticsFromMethod(Action action)
         {
-            long before = GC.GetTotalMemory(false);
             _stopwatch.Start();
 
             action();
 
             _stopwatch.Stop();
-            long after = GC.GetTotalMemory(false);
-
-            _memoryUsed = (after - before) / (1024 * 1024);
             _timeWork = _stopwatch.Elapsed;
             _stopwatch.Reset();
-            GC.Collect();
         }
 
-        private void ResetItems()
+        public static string GetStatistics(SortingStatistics<T> statistics) =>
+            statistics.GetStatistics();
+
+        public string GetStatistics() => _information.ToString();
+
+        public void StartCollectingStatistics()
         {
-            _algorithm.ResetCollection(_items);
+            for (int i = MinThreadsCount; i <= MaxThreadsCount; i += _stepIterationThreads)
+            {
+                _algorithm.Threads = i;
+                StatisticsFromMethod(() => _algorithm.StartMultiThreadingSort());
+
+                _threadsTimeMemory.Add(i, _timeWork);
+
+                _information.AppendLine($"Threads: {i}\n" +
+                                        $"\tTime work: {_timeWork.TotalMilliseconds}ms\n");
+
+                _algorithm.ResetCollection(_items);
+            }
+
         }
+
+        public int BetterThreadsCountOfTime() =>
+            _threadsTimeMemory.MinBy(pair => pair.Value).Key;
     }
 }
